52af0889d838686b7dd953bdcaf34889a619e550
Alex Marchand <alexmarchandrs@gmail.com>
2017-01-01 23:25:32 -0200

    added months/weeks/days logic and implemented css for print

---
+++ diaSemana.rb
@@ -0,0 +1,8 @@
+class DiaSemana
+  attr_accessor :name, :diaUtil
+
+  def initialize(name, diaUtil)
+    @name = name
+    @diaUtil = diaUtil
+  end
+end
--- main.rb
+++ main.rb
@@ -5,7 +5,6 @@ html_file = File.basename(erb_file, '.erb') #=>"index.html"
 
 erb_str = File.read(erb_file)
 
-@teste = "sei la"
 renderer = ERB.new(erb_str)
 result = renderer.result()
 
